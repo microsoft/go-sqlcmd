2d51ba2eb022be77d9f56959025e8842cb608515
stuartpa <stuartpa@hotmail.com>
2022-09-07 03:41:11 -0700

    Final .rb before submitting to homebrew

--- release/mac/homebrew/sqlcmd.rb
+++ release/mac/homebrew/sqlcmd.rb
@@ -1,21 +1,25 @@
 class Sqlcmd < Formula
-  desc "Sqlcmd for Microsoft(R) SQL Server(R)"
+  desc "Microsoft SQL Server command-line interface"
   homepage "https://github.com/microsoft/go-sqlcmd"
-  url "https://github.com/microsoft/go-sqlcmd/releases/download/v0.8.0/sqlcmd-v0.8.0-darwin-x64.tar.bz2"
-  version "0.8.0"
-  sha256 "f68eebdaca706f92f57ff3da2504834f146ab2a112920f16170cb86e08372989"
+  url "https://github.com/microsoft/go-sqlcmd.git",
+  tag:      "v0.8.1",
+  revision: "4e0b95ce49b8164c6496ca1b7a85fa57734fef4c"
+  license "MIT"
+
+  depends_on "go" => :build
 
   def check_eula_acceptance?
     if ENV["ACCEPT_EULA"] != "y" && ENV["ACCEPT_EULA"] != "Y"
       puts "The license terms for this product can be downloaded from"
-      puts "https://github.com/microsoft/go-sqlcmd/blob/main/LICENSE. "
+      puts "https://github.com/microsoft/go-sqlcmd/blob/main/LICENSE."
       puts "By entering 'YES', you indicate that you accept the license terms."
       puts ""
       loop do
         puts "Do you accept the license terms? (Enter YES or NO)"
-        accept_eula = STDIN.gets.chomp
+        accept_eula = $stdin.gets.chomp
         if accept_eula
           break if accept_eula.casecmp("YES").zero?
+
           if accept_eula.casecmp("NO").zero?
             puts "Installation terminated: License terms not accepted."
             return false
@@ -36,9 +40,9 @@ class Sqlcmd < Formula
   def install
     return false unless check_eula_acceptance?
 
-    chmod 0444, "/bin"
+    ENV["CGO_ENABLED"] = "0"
 
-    cp_r ".", prefix.to_s
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/sqlcmd"
   end
 
   test do
