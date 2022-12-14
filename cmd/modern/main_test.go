41d640c7c996d409ff6338a4221d6d20b5d99320
stuartpa <stuartpa@hotmail.com>
2022-12-14 04:32:28 -0800

    Saving changes

--- cmd/modern/main_test.go
+++ cmd/modern/main_test.go
@@ -27,6 +27,7 @@ func TestInitializeCallback(t *testing.T) {
 
 func TestDisplayHints(t *testing.T) {
 	buf := test.NewMemoryBuffer()
+	defer buf.Close()
 	outputter = output.New(output.Options{StandardWriter: buf})
 	displayHints([]string{"This is a hint"})
 	assert.Equal(t, pal.LineBreak()+
