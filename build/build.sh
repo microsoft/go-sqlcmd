39f0ecc3c0b03563a8b7921e5e37ba1a815d1ca5
davidshi <david.shiflet@microsoft.com>
2023-01-25 16:43:21 -0600

    Generate NOTICE file for licenses

--- build/build.sh
+++ build/build.sh
@@ -2,3 +2,8 @@
 scriptdir=`dirname $0`
 versionTag=`git describe --tags --abbrev=0`
 go build -o $scriptdir/../sqlcmd -ldflags="-X main.version=$versionTag" $scriptdir/../cmd/modern
+
+go install github.com/google/go-licenses@latest
+go-licenses report github.com/microsoft/go-sqlcmd/cmd/modern --template build/NOTICE.tpl --ignore github.com/microsoft > $scriptDir/notice.txt
+cat $scriptDir/NOTICE.header $scriptDir/notice.txt > $scriptDir/../NOTICE.md
+rm $scriptDir/notice.txt
