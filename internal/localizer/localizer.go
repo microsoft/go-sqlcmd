52073f7adcf95cab3bb36a79d87000b50d537b3b
Apoorv Deshmukh <apdeshmukh@microsoft.com>
2023-02-14 10:27:40 +0530

    Add localization support for go-sqlcmd (#207)
    Add localization support for go-sqlcmd
    
    This commit adds localization framework and support for
    languages en-us, de-de, fr-fr, es-es, pt-br, ko-kr, zh-cn, zh-tw,
    it-it, ja-jp, ru-ru, zh-hans, zh-hant. Translations have been taken
     from ODBC sqlcmd.rll.lcl for corresponding languages.

---
+++ internal/localizer/localizer.go
@@ -0,0 +1,56 @@
+package localizer
+
+import (
+	// Import the internal/translations so that its init() function
+	// is run. It's really important that we do this here so that the
+	// default message catalog is updated to use our translations
+	// *before* we initialize the message.Printer instances below.
+
+	"fmt"
+	"os"
+	"strings"
+
+	_ "github.com/microsoft/go-sqlcmd/internal/translations"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+)
+
+var Translator *message.Printer
+
+var supportedLanguages = map[string]string{
+	"de-de":   "de-DE",
+	"fr-fr":   "fr-FR",
+	"en-us":   "en-US",
+	"zh-hans": "zh-CN",
+	"zh-cn":   "zh-CN",
+	"zh-hant": "zh-TW",
+	"zh-tw":   "zh-TW",
+	"it-it":   "it-IT",
+	"ja-jp":   "ja-JP",
+	"ko-kr":   "ko-KR",
+	"pt-br":   "pt-BR",
+	"ru-ru":   "ru-RU",
+	"es-es":   "es-ES",
+}
+
+// init() initializes the language automatically
+// based on env var SQLCMD_LANG which expects language
+// tag such as en-us, de-de, fr-ch, etc.
+func init() {
+	localeName := strings.ToLower(os.Getenv("SQLCMD_LANG"))
+	if _, ok := supportedLanguages[localeName]; !ok {
+		localeName = "en-us"
+	}
+	Translator = message.NewPrinter(language.MustParse(supportedLanguages[localeName]))
+}
+
+// Errorf() is wrapper function to create localized errors
+func Errorf(format string, a ...any) error {
+	errMsg := Translator.Sprintf(format, a...)
+	return fmt.Errorf(errMsg)
+}
+
+// Sprintf() is wrapper function to create localized string
+func Sprintf(key message.Reference, args ...interface{}) string {
+	return Translator.Sprintf(key, args...)
+}
---
+++ internal/translations/translations.go
@@ -0,0 +1,3 @@
+package translations
+
+//go:generate gotext -srclang=en-US update -out=catalog.go -lang=en-US,de-DE,fr-FR,zh-CN,zh-TW,it-IT,ja-JP,ko-KR,pt-BR,ru-RU,es-ES github.com/microsoft/go-sqlcmd/cmd/modern github.com/microsoft/go-sqlcmd/cmd/sqlcmd github.com/microsoft/go-sqlcmd/pkg/sqlcmd
