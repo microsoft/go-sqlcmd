d7f4ec56840b950318a0a515605bc4845d4456bf
Stuart Padley <stuartpa@hotmail.com>
2023-03-10 00:50:19 -0800

    Fix is with SQLCMD_PASSWORD in pipelines

--- cmd/modern/root/query_test.go
+++ cmd/modern/root/query_test.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"github.com/microsoft/go-sqlcmd/cmd/modern/root/config"
 	"github.com/microsoft/go-sqlcmd/internal/cmdparser"
-	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
@@ -42,11 +41,7 @@ func setupContext(t *testing.T) {
 	// we'll use trusted auth
 	if os.Getenv("SQLCMDPASSWORD") != "" &&
 		os.Getenv("SQLCMDUSER") != "" {
-
-		// sqlcmd uses the SQLCMD_PASSWORD env var, but the tests use the
-		// SQLCMDPASSWORD env var
-		err := os.Setenv("SQLCMD_PASSWORD", os.Getenv("SQLCMDPASSWORD"))
-		assert.Nil(t, err)
+		
 		cmdparser.TestCmd[*config.AddUser](
 			fmt.Sprintf("--name user1 --username %s",
 				os.Getenv("SQLCMDUSER")))
