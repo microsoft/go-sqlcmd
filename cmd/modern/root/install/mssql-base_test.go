d9725002446cb7693de1443feb3bfdf8dd79812f
Apoorv Deshmukh <apdeshmukh@microsoft.com>
2023-03-20 23:10:42 +0530

    Add unit test for extractUrl

--- cmd/modern/root/install/mssql-base_test.go
+++ cmd/modern/root/install/mssql-base_test.go
@@ -44,3 +44,20 @@ func TestGetDbNameIfExists(t *testing.T) {
 		assert.Equal(t, testcase.expectedNonIdentifierOp, dbnameAsNonIdentifier, "Unexpected database name as non-identifier")
 	}
 }
+
+func TestExtractUrl(t *testing.T) {
+	type test struct {
+		inputURL    string
+		expectedURL string
+	}
+
+	tests := []test{
+		{"https://example.com/testdb.bak,myDbName", "https://example.com/testdb.bak"},
+		{"https://example.com/testdb.bak", "https://example.com/testdb.bak"},
+		{"https://example.com,", "https://example.com,"},
+	}
+
+	for _, testcase := range tests {
+		assert.Equal(t, testcase.expectedURL, extractUrl(testcase.inputURL), "Extracted URL does not match expected URL")
+	}
+}
