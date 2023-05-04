4404bae81c1e3490be6b7dc9d06de1214f9808f3
Jyotika Garg <jyotikagarg@microsoft.com>
2023-05-04 13:17:44 +0530

    adding log for linux run fail

--- pkg/sqlcmd/commands_test.go
+++ pkg/sqlcmd/commands_test.go
@@ -220,6 +220,7 @@ func TestConnectCommand(t *testing.T) {
 	password := ""
 	username := ""
 	if canTestAzureAuth() {
+		fmt.Printf("Inside canTestAzureAuth")
 		authenticationMethod = "-G " + azuread.ActiveDirectoryDefault
 	}
 	if c.Password != "" {
