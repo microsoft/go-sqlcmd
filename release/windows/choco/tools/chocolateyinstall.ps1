7fd36b86a40d182de59a7ef6883aea9bf3685b2e
Apoorv Deshmukh <apdeshmukh@microsoft.com>
2023-08-16 21:19:59 +0530

    Update nupkg and install script

--- release/windows/choco/tools/chocolateyinstall.ps1
+++ release/windows/choco/tools/chocolateyinstall.ps1
@@ -1,20 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = '{{DownloadUrl}}'
 $url64      = 'https://github.com/microsoft/go-sqlcmd/releases/download/v1.3.0/sqlcmd-x64_1.3.0-1.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
-  url           = $url
   url64bit      = $url64
-
   softwareName  = 'sqlcmd*'
-
-  checksum      = '{{Checksum}}'
-  checksumType  = '{{ChecksumType}}'
   checksum64    = '7ecae5c7c20c0cb0e44e1b6e5e3a2c2064a4b6fc2bda07622e33aae6d60fd83e'
   checksumType64= 'sha256'
 
@@ -23,20 +17,3 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
