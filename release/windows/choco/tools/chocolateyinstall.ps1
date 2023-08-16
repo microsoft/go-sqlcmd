3f2c6f29969dc0f4b9b1b845156d4b278d099d89
Apoorv Deshmukh <apdeshmukh@microsoft.com>
2023-08-16 20:40:30 +0530

    Choco package for v1.3.0

--- release/windows/choco/tools/chocolateyinstall.ps1
+++ release/windows/choco/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = '{{DownloadUrl}}'
-$url64      = 'https://download.microsoft.com/download/d/4/4/d4403a51-2ab7-4ea8-b850-d2710c5e1323/sqlcmd_0.8.1-1.msi'
+$url64      = 'https://github.com/microsoft/go-sqlcmd/releases/download/v1.3.0/sqlcmd-x64_1.3.0-1.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -15,7 +15,7 @@ $packageArgs = @{
 
   checksum      = '{{Checksum}}'
   checksumType  = '{{ChecksumType}}'
-  checksum64    = '03587762932D5A66ACFE15D306FE14645D53BC61162B4DA0D9AF29B4A8A1550D'
+  checksum64    = '7ecae5c7c20c0cb0e44e1b6e5e3a2c2064a4b6fc2bda07622e33aae6d60fd83e'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
@@ -23,9 +23,6 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs
-
-
-
 
 
 
