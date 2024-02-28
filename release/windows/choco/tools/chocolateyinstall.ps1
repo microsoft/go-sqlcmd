e0643c8f2e79ab145e2d06b23801a6f5c59c87f0
Stuart Padley <stuartpa@hotmail.com>
2024-02-28 13:11:31 -0800

    1.6.0

--- release/windows/choco/tools/chocolateyinstall.ps1
+++ release/windows/choco/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/microsoft/go-sqlcmd/releases/download/v1.5.0/sqlcmd-x64_1.5.0-1.msi'
+$url64      = 'https://github.com/microsoft/go-sqlcmd/releases/download/v1.6.0/sqlcmd-x64_1.6.0-1.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   fileType      = 'MSI'
   url64bit      = $url64
   softwareName  = 'sqlcmd*'
-  checksum64    = 'fe1a7045fd6c78d33666b17b12b86b4b1ee414cb49fc093130aa8b1294aadf10'
+  checksum64    = '89599bcfa6dce0a81975353b67a250f0072ec5aed45e73039d8c85874d7e2d85'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
